9ad8c180e5f6fd0ac01cf63e0ecb050d53a372f1
donal007 <150226333+donal007@users.noreply.github.com>
2024-09-25 13:31:10 +0530

    Add files via upload

---
+++ Donall FSD/JS2/script.js
@@ -0,0 +1,94 @@
+//if(5<10){
+    
+  //  console.log("5 is less");
+//}
+//else{
+  //  console.log("10 is greater");
+//}
+
+//let age = 10;
+//if(age>18){
+  //  console.log(" Eligible for Driving License");
+//}
+//else{
+    //console.log("Not Eligible for Driving License");
+//}
+
+//let time = 21
+
+//if(time<=12){
+  //  console.log("Good Morning")
+
+//} 
+//else if(time>12 && time<=15){
+  //  console.log("Good Afternoon")
+//}
+
+//else if(time>15 && time<=19){
+  //  console.log("Good Evening")
+//}
+
+//else{
+  //  console.log("Good Night"); 
+//}
+    
+   
+//let a = 18;
+//(a<20)?console.log("a is less than 20"):console.log("a is greater than 20");
+
+//console.log("1");
+//console.log("2");
+//console.log("3");
+//console.log("4");
+//console.log("5");
+
+
+//for(let i=1;i<=10;i++){
+  //  console.log(i);
+//}
+
+//for(let i=0;i<=10;i++){
+  //  console.log(i);
+//}
+
+
+//let fruits = ["Apple","Banana","Mango","Orange"];
+//for(let i=0;i<fruits.length;i++){
+   // console.log(fruits[i]);
+
+//}
+
+//let numbers = [1,2,3,4,5,6]
+//for(let i=0;i<numbers.length;i++){
+  //  if(numbers[i]%2==0){
+    //    console.log("Even Number");
+    //}
+    //else{
+      //  console.log("Odd Number");
+    //}
+    
+//}
+
+
+//Find the sum of the Array
+//let values = [1,2,3,4,5,6,7,8,9]
+//let sum = 0;
+//for(let i=0;i<values.length;i++){
+  //  sum = sum + values[i];
+//}
+//console.log(sum);
+
+//Find the product of the Array
+//let values = [1,2,3,4,5]
+//let product = 1;
+//for(let i=0;i<values.length;i++){
+  //  product = product * values[i];
+//}
+//console.log(product);
+
+//Reverse the String
+const str = "Hello Nirmala!"
+
+for(let i=str.length-1;i>=0;i--){
+    console.log(str[i]);
+}
